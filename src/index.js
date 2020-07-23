c0943a9ce8ec8ddb51b6a092e03a0f9706b62ba0
Hector <hector.rs187@gmail.com>
2020-07-22 20:33:18 -0500

    Primer commit

--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@
 // https://babeljs.io/docs/en/babel-preset-env
 // https://webpack.js.org/guides/output-management/#cleaning-up-the-dist-folder
 
-import {
-    saludar
-} from "./js/componentes.js";
 import "./styles.css";
+import {
+    Todo,
+    TodoList
+} from './classes';
+import {
+    crearTodoHtml
+} from "./js/componentes";
 
-const nombre = "Hector";
+export const todoList = new TodoList();
 
-saludar(nombre);
+todoList.todos.forEach(crearTodoHtml);
